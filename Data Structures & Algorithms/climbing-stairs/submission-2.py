677da22cfe776afb3bd9af7ccf497b07c277dc34
Peter Phan <49101240+peterphann@users.noreply.github.com>
2026-09-15 22:16:11 -0700

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,8 @@
+from functools import lru_cache
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a, b = 1, 1
+        for i in range(n - 1):
+            a, b = b, a + b
+        return b
