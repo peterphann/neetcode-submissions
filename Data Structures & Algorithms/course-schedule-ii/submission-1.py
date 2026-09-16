15afa83fd2dc9686f79cbe57fcc552348c96fbca
Peter Phan <49101240+peterphann@users.noreply.github.com>
2026-09-16 01:04:56 -0700

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adjList = [set() for _ in range(numCourses)]
+        indegrees = [0] * numCourses
+
+        for course, prereq in prerequisites:
+            adjList[prereq].add(course)
+            indegrees[course] += 1
+        
+        res = []
+        queue = deque([c for c in range(numCourses) if indegrees[c] == 0])
+        
+        while queue:
+            snapshot = sorted(queue.copy())
+            queue = []
+
+            for curr in snapshot:
+                res.append(curr)
+
+                for nei in adjList[curr]:
+                    indegrees[nei] -= 1
+                    if indegrees[nei] == 0:
+                        queue.append(nei)
+        
+        return res if len(res) == numCourses else []
+
